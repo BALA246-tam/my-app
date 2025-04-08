1ee308a4921a28fd0021f0be75a245fa3c865aa5
BALA246-tam <subramaniballa87@gmail.com>
2025-04-08 21:39:36 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,5 @@
-function callBackend() {
-  fetch('/.netlify/functions/app')
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById("response").textContent = data.message;
-    });
-}
+fetch('/.netlify/functions/app')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("response").textContent = data.message;
+  });
