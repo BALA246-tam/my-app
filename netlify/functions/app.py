e5f66d54638592833bbbb7f5f61068a25a00b10b
BALA246-tam <subramaniballa87@gmail.com>
2025-04-08 21:36:20 +0530

    Update app.py

--- netlify/functions/app.py
+++ netlify/functions/app.py
@@ -1,14 +1,5 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def handler():
-    return jsonify(message="Hello from Flask Backend!")
-
-# This makes the function work with Netlify
-def handler(event, context):
-    from flask_lambda import FlaskLambda
-    flask_lambda = FlaskLambda(app)
-    return flask_lambda(event, context)
-  
+fetch('/.netlify/functions/app')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("response").textContent = data.message;
+  });
